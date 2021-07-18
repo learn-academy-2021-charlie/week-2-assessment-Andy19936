ae869c8f3eba38510570ae61f44adbf5014bac89
Andy19936 <andy19936@gmail.com>
2021-07-18 13:18:25 -0700

    all done

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -16,7 +16,17 @@
 
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
+// create a function named divBy3 
+// use a conditional statement
+// using the modulo to decide if the parameter is divisible by 3
+// write out my returns
+// review test challenges for jest syntax
+// input all my test parameters
+// lets see how this goes 
+
 // a) Create a test with expect statements for each of the variables provided.
+
+// b) Create the function that makes the test pass.
 
 var num1 = 15
 // Expected output: "15 is divisible by three"
@@ -27,13 +37,37 @@ var num2 = 0
 var num3 = -7
 // Expected output: "-7 is not divisible by three"
 
+describe("The divBy3 function.",()=>{
+    test("takes a number and determines if it is evenly divisible by 3",() => {
+        expect(divBy3(15)).toEqual(`15 is divisible by three`)
+        expect(divBy3(0)).toEqual(`0 is divisible by three`)
+        expect(divBy3(-7)).toEqual(`-7 is not divisible by three`)
+    })
+})
+
+const divBy3 = (number) =>{
+    if(number % 3 == 0){
+        return`${number} is divisible by three`
+        } else {
+        return `${number} is not divisible by three`
+        }
+}
+console.log(divBy3(3))
 
 
-// b) Create the function that makes the test pass.
 
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+
+// create a function were going with allCaps *RIP MFDOOM
+// the function is going to take in an array and return a new array w
+// use map()
+// use .touppercase() to capitalize the new array in all caps 
+// hope it works
+// realize only the first letter needs to be capitalized.. back to the drawing board
+// sticking wiht .map using split 
+
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -47,6 +81,21 @@ var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction
 
 // b) Create the function that makes the test pass.
 
+describe("The allCaps function.",()=>{
+    test("takes an array and returns a new array that has all been capitalized",() => {
+        expect(allCaps(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(allCaps(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+    })
+})
+
+const allCaps = (array) =>{
+    let capitalizedArray = array.map(value =>{
+        return value[0].toUpperCase() + value.substring(1)
+    })
+    return capitalizedArray
+}
+var testArray = ["all", "happy", "people"]
+ console.log(allCaps(testArray))
 
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
@@ -60,6 +109,34 @@ var vowelTester2 = "academy"
 var vowelTester3 = "challenge"
 // Expected output: 2
 
-
-
 // b) Create the function that makes the test pass.
+
+
+// assign your function a name were going with firstVowel
+// use split to split my string into an array
+// use filter to log the first instcance of vowels 
+// give a return of the vowel
+// change of plans going to use charAt and a for loop instead
+// dedicate all your brain juice to classes and forget for loops
+// review and play with for loop for an hour -_-
+// spend another hour changing things up reach out to Austin and Sarah
+// reach desired outcome! Thanks for the help guys!
+
+describe("The firstVowel function.",()=>{
+    test("takes a string and tells you the index of the first occurence of a vowel",() => {
+        expect(firstVowel(vowelTester1)).toEqual(1)
+        expect(firstVowel(vowelTester2)).toEqual(0)
+        expect(firstVowel(vowelTester3)).toEqual(2)
+    })
+})
+
+const firstVowel = (string)=>{
+    
+    for(let i=0;i <string.length; i++){ 
+        if(string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" ||string.charAt(i) == "o" ||string.charAt(i) == "u"){
+            return i
+        }
+    }  
+}
+var newString = "lalaas"
+console.log(firstVowel(newString))
--- interview-questions.md
+++ interview-questions.md
@@ -8,49 +8,49 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is iteration?
 
-  Your answer:
+  Your answer: iteration means you are doing an action a certain amount of times or until your condition is met 
 
-  Researched answer:
+  Researched answer: process of performing a specific action a set amount of times or until a condition is met
 
 
 
 2. The built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-  Your answer:
+  Your answer: maps predefined parameters I believe are value,index, and array and I belive you only need value
 
-  Researched answer:
+  Researched answer: The parameters are indeed value,index and array. The required parameter .map() uses is value and that is because it is extracting the "values" at every index to refactor into a new array.
 
 
 
 3. What is object destructuring?
 
-  Your answer:
+  Your answer: object destructuring is accessing specific values within an object 
 
-  Researched answer:
+  Researched answer: Object destructuring is unpacking values from an object and assigning them specific variables.
 
 
 
 4. What is the difference between an object and a class?
 
-  Your answer:
+  Your answer: A class is the framework an object uses to base itself off of.
 
-  Researched answer:
+  Researched answer: A class defines object properties and default values for those properties. An object is an instance of a class.
 
 
 
 5. Why would you use the method super()?
 
-  Your answer:
+  Your answer: The method super() brings in all the properties created by the parent class and inputs them into a child class.
 
-  Researched answer:
+  Researched answer: The super method passes attributes from the parent classes constructor.
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: I have no idea what hoisting means in JavaScript im going to assume it involves pushing your JS code? Let me research the subject and I will present you with a refined answer on the lower line.
 
-  Researched answer:
+  Researched answer: Hoisting is saving declarations of variables and functions and "pre-storing" similar to moving them to the top of your code but without actually moving it. Useful for calling functions before declaring them in your code.
 
 
 
@@ -58,12 +58,13 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-1. React:
+1. React: React is a JavaScript library used for building user interface.
 
-2. React lifecycle methods:
+2. React lifecycle methods: The series of events happening from the beginning to the end of a react component: mounting, updating, unmounting. 
 
-3. React state:
+3. React state: An object that stores a components data and determines how the component renders and behaves.
 
-4. Component invocation/call:
+4. Component invocation/call: Requires two steps first the component has to be imported then it is called in a return. 
+When a component is first called or any changes are made to the component the render function is called which builds jsx that react puts on the page 
 
-5. DOM Events:
+5. DOM Events: Document Object Model "DOM" a signal that something has happened or is happening and can be triggered by a user or browser.
